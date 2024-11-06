097b0b1535b4448e2ec7bc210655d14afdc6f173
Viper242 <isulgar@gmail.com>
2024-11-06 19:15:34 +0100

    Initial commit. Added permissions to each platform to use location.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FindMe_GO
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
